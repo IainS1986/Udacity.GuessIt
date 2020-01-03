03e2f0159f15d7dc83010c2823d387ed5043be0e
Iain <i_stanford@hotmail.com>
2020-01-03 14:49:39 +0000

    Lesson 26 (Optional)
    Was about to start doing the optional buzzing/virbation stuff, but can't be bothered to hook up my actual device. no real need to I think as I understand ewhats going on with LiveData -> ViewModel -> View etc etc to get Mvvm setup. Not quite as nice as Xamarin+MvvmCross but its not that different either.

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -18,6 +18,19 @@ class GameViewModel : ViewModel() {
         const val ONE_SECOND = 1000L
         // This is the total time of the game
         const val COUNTDOWN_TIME = 10000L
+
+        // BUZZ timings for vibration
+        private val CORRECT_BUZZ_PATTERN = longArrayOf(100, 100, 100, 100, 100, 100)
+        private val PANIC_BUZZ_PATTERN = longArrayOf(0, 200)
+        private val GAME_OVER_BUZZ_PATTERN = longArrayOf(0, 2000)
+        private val NO_BUZZ_PATTERN = longArrayOf(0)
+    }
+
+    enum class BuzzType(val pattern: LongArray) {
+        CORRECT(CORRECT_BUZZ_PATTERN),
+        GAME_OVER(GAME_OVER_BUZZ_PATTERN),
+        COUNTDOWN_PANIC(PANIC_BUZZ_PATTERN),
+        NO_BUZZ(NO_BUZZ_PATTERN)
     }
 
     // The current word
